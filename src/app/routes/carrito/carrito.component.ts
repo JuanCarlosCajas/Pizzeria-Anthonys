a8b33f42dd1472caaf3e758bb349782298fc789e
unknown <os8602231@gmail.com>
2024-07-12 15:02:35 -0500

    funcionando

--- src/app/routes/carrito/carrito.component.ts
+++ src/app/routes/carrito/carrito.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { CartService } from '../../cart.service';
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css'
 })
 export class CarritoComponent {
+  constructor(private cartService: CartService) {}
+  addToCart(product: string, price: number) {
+    this.cartService.addToCart({ name: product, price: price, quantity: 1 });
+  }
 
 }
--- src/app/routes/carrito/complementos/complementos.component.ts
+++ src/app/routes/carrito/complementos/complementos.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { CartService } from '../../../cart.service';
 @Component({
   selector: 'app-complementos',
   templateUrl: './complementos.component.html',
   styleUrl: './complementos.component.css'
 })
 export class ComplementosComponent {
+  constructor(private cartService: CartService) {}
+  addToCart(product: string, price: number) {
+    this.cartService.addToCart({ name: product, price: price, quantity: 1 });
+  }
 
 }
